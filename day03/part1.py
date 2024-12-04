24b86294767e9205e46cb994310e972a520e466c
Alex Terry <28978970+AlexPTerry@users.noreply.github.com>
2024-12-04 11:41:00 +0000

    Restored day 3 part 1, cleaned up part 2

--- day03/part1.py
+++ day03/part1.py
@@ -13,45 +13,11 @@ def calc_multiples(str):
     matches = re.findall(pattern, str)
     return sum(int(x) * int(y) for x, y in matches)
 
-def find_str_index(text, str):
-    return text.find(str)
-
-def find_next_do(text):
-    return text.find("do()")
-
-def find_next_dont(text):
-    index = text.find("don't()")
-    if index == -1:
-        return len(text)
-    return text.find("don't()")
-
 def main():
     input_file = get_input()
-    
-    total = 0
-    text = input_file
-    on = True
-    
-    while text:
-        if on:
-            off_from = find_next_dont(text)
-            start, end = (text[:off_from], text[off_from + 7:])
-            total += calc_multiples(start)
-            text = end
-            on = False
-        else:
-            on_from = find_next_do(text)
-            if on_from == -1:
-                break
-            text = text[on_from + 4:]
-            on = True
-            
+    total = calc_multiples(input_file)
     print(total)
             
             
-            
-            
-    
-
 if __name__ == "__main__":
     main()
--- day03/part2.py
+++ day03/part2.py
@@ -1,31 +1,54 @@
 import os
-import numpy as np
+import re
 
 def get_input(name="input.dat"):
     current_directory = os.path.dirname(os.path.realpath(__file__))
     input_file_path = os.path.join(current_directory, name)
     with open(input_file_path, 'r') as file:
-        input_file = file.read().strip().split('\n')
+        input_file = file.read().strip()
     return input_file
 
-def is_safe(line):
-    nums = np.asarray(list(map(int, line.split(' '))))
-    for i in range(len(nums)): # Removing no number is safe => removing the first number is safe
-        diff = np.diff(nums[np.arange(len(nums)) != i])
-        if (np.all(diff >= 1) and np.all(diff <= 3) or np.all(diff <= -1) and np.all(diff >= -3)):
-            return True
-    return False
+def calc_multiples(str):
+    pattern = r"mul\((\d+),(\d+)\)"
+    matches = re.findall(pattern, str)
+    return sum(int(x) * int(y) for x, y in matches)
+
+def find_next_do(text):
+    return text.find("do()")
+
+def find_next_dont(text):
+    index = text.find("don't()")
+    if index == -1:
+        return len(text)
+    return text.find("don't()")
 
 def main():
     input_file = get_input()
     
-    total_safe = 0
-    for line in input_file:
-        if is_safe(line):
-            total_safe += 1
+    total = 0
+    text = input_file
+    on = True
+    
+    while text:
+        if on:
+            off_from = find_next_dont(text)
+            start, end = (text[:off_from], text[off_from + 7:])
+            total += calc_multiples(start)
+            text = end
+            on = False
+        else:
+            on_from = find_next_do(text)
+            if on_from == -1:
+                break
+            text = text[on_from + 4:]
+            on = True
             
-    print(total_safe)
-
+    print(total)
+            
+            
+            
+            
+    
 
 if __name__ == "__main__":
     main()
